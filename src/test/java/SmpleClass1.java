4d2aabb199ec4b8f20786755946c26425d7961e9
Ludmila Berdila <ludmila.berdila@gmail.com>
2024-01-09 00:29:16 -0600

    3 pract

--- src/test/java/SmpleClass1.java
+++ src/test/java/SmpleClass1.java
@@ -1,6 +1,11 @@
 public class SmpleClass1 {
     public static void main(String[] args) {
-        System.out.println("hi Ludmila");
+        System.out.println("hi Ludmila    ");
         System.out.println("today is monday");
+        System.out.println("this is my second change");
+        System.out.println("3 practice");
+
+
+
     }
 }
